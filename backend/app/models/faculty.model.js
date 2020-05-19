e3967345bd834aad7a38da06883de2ca7e978cae
lea <learbiv@cs.colman.ac.il>
2020-05-19 17:20:43 +0300

    add Faculty Scraping

--- backend/app/models/faculty.model.js
+++ backend/app/models/faculty.model.js
@@ -1,3 +1,7 @@
+ 
+const rp = require('request-promise');
+const $ = require('cheerio');
+
 module.exports = mongoose => {
   
     var schema = mongoose.Schema(
@@ -16,24 +20,38 @@ module.exports = mongoose => {
      const Faculty = mongoose.model("Faculty", schema)
      const createFaculty = async ({facultyName}) => {
       const matches = await Faculty.find({facultyName}).exec();
-  
+       
       if (matches.length === 0) {
           return Faculty.create({facultyName});
       }
   };
 
 
+  const faculties =[];
 
-  const faculties = [{
-    facultyName: "Economics"}, {facultyName: "Computer Science"},{facultyName: "Laws"},{facultyName: "Humen Resources"}
-  ];
-  faculties.forEach((element)=>(async () => {
-    await createFaculty(element);
-})())
+  // const faculties = [{
+  //   facultyName: "Economics"}, {facultyName: "Computer Science"},{facultyName: "Laws"},{facultyName: "Humen Resources"}
+  // ];
+  // faculties.forEach((element)=>(async () => {        
+  //             await createFaculty(element);
+            // })())
+// console.log(faculties);
 
+const  url='http://english.colman.ac.il/staff_members/'
+rp(url) 
+ .then(function(html) { 
+      $('.faculty-name', html).each(function() { 
+        faculties.push({facultyName: $(this).text()})
+         });
 
-
-    
+        console.log(faculties);
+        faculties.forEach((element)=>(async () => {        
+          await createFaculty(element);
+        })())
+  }) 
+    .catch(function(err){
+  //      handle error   
+ });
 
     return Faculty;
   };
